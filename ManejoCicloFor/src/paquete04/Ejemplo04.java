69d1a3bad7855e876b692705148f893b864c71c9
JDavid <jorgedaviddiaz62@gmail.com>
2024-12-04 12:23:52 -0500

    Ejemplos

--- ManejoCicloFor/src/paquete04/Ejemplo04.java
+++ ManejoCicloFor/src/paquete04/Ejemplo04.java
@@ -5,6 +5,7 @@
  */
 package paquete04;
 
+import java.util.Scanner;
 import paquete01.*;
 
 /**
@@ -17,9 +18,26 @@ public class Ejemplo04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    
+        int maximot;
+        int numeroTablas;
         
-        for (int i = 20; i >= 0; i-=2) {
-            System.out.printf("%d\n", i);
+        System.out.println("Ingrese el numero de tablas");
+        numeroTablas = entrada.nextInt();
+        System.out.println("Ingrese el maximo de la tabla");
+        maximot = entrada.nextInt();
+        
+        int numero1 = 1;
+        while (numero1 <= numeroTablas) {
+            int numero2 = 1;
+           while (numero2 <= maximot);{
+           System.out.println(numero1 + "x" + numero2 + "=" + (numero1 * numero2));
+           numero2++;
+           }
+
+            System.out.println("_____________");
+            numero1++;
         }
       
     }
